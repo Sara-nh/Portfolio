6c4f9c93bf37e97d6f846ada04efce855d514d2e
Sara-nh <sara_n_h@yahoo.com>
2021-06-18 07:33:26 -0700

    skillSection

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-$('#slides').superslides({
-	animation: 'fade',
-	play: 5000,
-	pagination: false,
-});
-});
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false,
+	});
+
 
 var typed = new Typed(".typed", {
   strings: ["Software Developer", "Web Developer"],
@@ -13,3 +13,60 @@ var typed = new Typed(".typed", {
   startDelay: 1000,
   showCursor: false,
 });
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 4,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        938:{
+            items:4
+        }
+    }
+});
+
+
+var skillsTopOffset= $(".skillsSection").offset().top;
+//console.log(skillsTopOffset);
+$(window).scroll(function(){
+
+console.log(window.pageYOffset);
+if(window.pageYOffset>skillsTopOffset-$(window).height()+200){
+
+
+  $('.chart').easyPieChart({
+      easing:'easeInOut',
+      trackColor: false,
+      scaleColor: false,
+      barColor: '#fff',
+      lineWidth: 4,
+      size: 152,
+      onStep(from, to, percent){
+        $(this.el).find('.percent').text(Math.round(percent));
+      }
+  });
+
+  
+
+}
+
+
+});
+	
+
+});
+
+
+
+
+
+
